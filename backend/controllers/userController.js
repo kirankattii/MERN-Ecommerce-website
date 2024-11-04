d7585b43b450fd8c1467dffc821cfb3786d5797f
kiran katti <kirankatti550@gmail.com>
2024-11-04 08:42:19 +0530

    changed bcryptjs

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import userModel from "../models/userModel.js"
 import validator from "validator"
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 import jwt from "jsonwebtoken"
 
 const createToken = (id) => {
@@ -77,12 +77,12 @@ const adminLogin = async (req, res) => {
 			email === process.env.ADMIN_EMAIL &&
 			password === process.env.ADMIN_PASSWORD
 		) {
-			const token = jwt.sign(email+password, process.env.JWT_SECRET)
+			const token = jwt.sign(email + password, process.env.JWT_SECRET)
 			res.json({ success: true, token })
 		} else {
 			res.json({ success: false, message: "Invalid Credentials" })
 		}
-	} catch (error) {}
+	} catch (error) { }
 }
 
 export { loginUser, registerUser, adminLogin }
